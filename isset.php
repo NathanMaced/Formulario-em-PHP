ef87acb863083e0b2407117a2252d9d849dc87b6
NathanMaced <nathanmacedo02@gmail.com>
2022-11-07 20:18:10 -0300

    Metodo ISSET

---
+++ isset.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulario - Recebendo dados - POST - Elemento checkbox - ISSET</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ pagina_recuperando_dados_post.php
@@ -0,0 +1,8 @@
+<?php 
+
+echo "Recuperando os dados - POST";
+echo "Nome: ".$_POST["nome"];
+echo "<br/>";
+echo "Senha: ".$_POST["senha"];
+
+ ?>
